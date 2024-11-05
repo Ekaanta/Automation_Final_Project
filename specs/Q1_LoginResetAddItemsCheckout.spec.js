6f2ec98254a898cfcd2e61fc96d7571263c295cc
Ekaanta <155002639+Ekaanta@users.noreply.github.com>
2024-11-05 22:32:23 +0600

    Update Q1_LoginResetAddItemsCheckout.spec.js

--- specs/Q1_LoginResetAddItemsCheckout.spec.js
+++ specs/Q1_LoginResetAddItemsCheckout.spec.js
@@ -4,20 +4,17 @@ describe('SauceDemo Login Tests', () => {
     it('should display an error message when trying to log in with locked_out_user', async () => {
       
         await browser.url('https://www.saucedemo.com/');
-
         const usernameInput = await $('#user-name');
         await usernameInput.setValue('locked_out_user');
-
         const passwordInput = await $('#password');
         await passwordInput.setValue('secret_sauce');
-
         const loginButton = await $('#login-button');
         await loginButton.click();
-
         const errorMessage = await $('.error-message-container');
         const errorText = await errorMessage.getText();
 
         expect(errorText).to.include('Sorry, this user has been locked out.');
+        //await new Promise(resolve => setTimeout(resolve, 1000));
     });
 
 }); //Run
